df23d89b336801eb4df074a5cdcf999d884d9508
TonatiuhQbit <salvtonatiuhg@gmail.com>
2017-09-01 14:26:44 -0500

    Update ArbolAVL.h
    Template AVL Tree in C++.Deletion algorithm will be implemented as soon as possible.There's still a few changes to be made.

--- ArbolAVL.h
+++ ArbolAVL.h
@@ -1,8 +1,8 @@
 #include<iostream>
 /*
-  Para guardar la condiciÛn de equilbrio en el ·rbol, la altura de las ramas izquierda y derecha deben diferir a lo mucho en 1.
-  La condiciÛn de equilibrio puede verse afectada por la inserciÛn e eliminaciÛn de un nodo con clave dada,por lo que se deben
-  hacer las rotaciones necesarias para que el ·rbol permanezca equilibrado.
+  Para guardar la condici√≥n de equilbrio en el √°rbol, la altura de las ramas izquierda y derecha deben diferir a lo mucho en 1.
+  La condici√≥n de equilibrio puede verse afectada por la inserci√≥n e eliminaci√≥n de un nodo con clave dada,por lo que se deben
+  hacer las rotaciones necesarias para que el √°rbol permanezca equilibrado.
 */
 template <typename S>
 struct Nodo{
@@ -16,32 +16,32 @@ template <class T>
 class ArbolAVL{
 	private :
 		Nodo<T> *raiz;
-		/*True en caso de que el ·rbol estÈ vacÌo.*/
+		/*True en caso de que el √°rbol est√© vac√≠o.*/
 		bool esVacio(){
 			return raiz == nullptr;
 		}
 		Nodo<T> *insercion(Nodo<T> *r,T d,bool cambioEnAltura){
-			/*Si el ·rbol es vacio, asignamos memoria a la raiz(o padre)*/
+			/*Si el √°rbol es vacio, asignamos memoria a la raiz(o padre)*/
 			if(r == nullptr){
 				r = new Nodo<T>;
 				r->dato = d;
 				r->izquierda = nullptr;
 				r->derecha = nullptr;
 				cambioEnAltura = true;
-			}else if(r->dato > d){ /*inserciÛn a la izquierda*/
+			}else if(r->dato > d){ /*inserci√≥n a la izquierda*/
 				Nodo<T> *nuevoIzq = insercion(r->izquierda,d,cambioEnAltura);
 				r->izquierda = nuevoIzq;
 				if(cambioEnAltura){
-					
+					 
 					switch(r->factorEquilibrio){
-						/*El padre cuenta ˙nicamente con el nodo izquierdo y un factorEquilibrio = -1. Al insertar el nuevo nodo el
-						  factor de equilibrio del padre ser· -2,violando por lo tanto la condiciÛn de equilibrio.Para reequilibrar 
-						  el ·rbol se debe hacer una rotaciÛn Izquierda-Izquierda(hijo con factorEquilibrio = -1) o Izquierda-Derecha
+						/*El padre cuenta √∫nicamente con el nodo izquierdo y un factorEquilibrio = -1. Al insertar el nuevo nodo el
+						  factor de equilibrio del padre ser√° -2,violando por lo tanto la condici√≥n de equilibrio.Para reequilibrar 
+						  el √°rbol se debe hacer una rotaci√≥n Izquierda-Izquierda(hijo con factorEquilibrio = -1) o Izquierda-Derecha
 						  (hijo con factorEquilibrio = 1).No hay cambio en la altura.
 						*/
 				
 						case -1:{
-							/*Caso Izquierda-Izquierda(RotaciÛn simple.Padre e hijo involucrados.) :
+							/*Caso Izquierda-Izquierda(Rotaci√≥n simple.Padre e hijo involucrados.) :
 							               PADRE(-2)                                  
 							             /         \                           
 							           HIJOI(-1)    nullptr  
@@ -51,7 +51,7 @@ class ArbolAVL{
 							if(r->izquierda->factorEquilibrio == -1){
 								r = rotacionIzquierdaIzquierda(r, r->izquierda); /*Pasamos la raiz y el hijo*/
 							}
-							/*Caso Izquierda-Derecha (RotaciÛn doble.Padre,hijo y nieto involucrados.):
+							/*Caso Izquierda-Derecha (Rotaci√≥n doble.Padre,hijo y nieto involucrados.):
 							               PADRE(-2)
 							              /         \
  									HIJOI(1)        nullptr         
@@ -78,7 +78,7 @@ class ArbolAVL{
 						}
 					}
 				}
-			}else if(r->dato < d){ /*inserciÛn a la derecha*/
+			}else if(r->dato < d){ /*inserci√≥n a la derecha*/
 				Nodo<T> *nuevoDer = insercion(r->derecha,d,cambioEnAltura);
 				r->derecha = nuevoDer;
 				if(cambioEnAltura){
@@ -91,18 +91,18 @@ class ArbolAVL{
 							break;
 						}
 						/*El padre no tiene hijos, al insertar el nuevo nodo a la derecha modificamos su factor de equilibrio.
-						  Hay un incremento en la altura del ·rbol. */
+						  Hay un incremento en la altura del √°rbol. */
 						case 0:{
 							r->factorEquilibrio = 1;
 							break;
 						}
-						/*El padre cuenta ˙nicamente con el nodo derecho y un factorEquilibrio = 1. Al insertar el nuevo nodo el
-						  factor de equilibrio del padre ser· 2,violando por lo tanto la condiciÛn de equilibrio.Para reequilibrar 
-						  el ·rbol se debe hacer una rotaciÛn Derecha-Derecha(hijo con factorEquilibrio = 1) o Derecha-Izquierda
+						/*El padre cuenta √∫nicamente con el nodo derecho y un factorEquilibrio = 1. Al insertar el nuevo nodo el
+						  factor de equilibrio del padre ser√° 2,violando por lo tanto la condici√≥n de equilibrio.Para reequilibrar 
+						  el √°rbol se debe hacer una rotaci√≥n Derecha-Derecha(hijo con factorEquilibrio = 1) o Derecha-Izquierda
 						  (hijo con factorEquilibrio = -1).No hay cambio en la altura.
 						*/
 						case 1:{
-							/*Caso Derecha-Derecha(RotaciÛn simple.Padre e hijo involucrados.) :
+							/*Caso Derecha-Derecha(Rotaci√≥n simple.Padre e hijo involucrados.) :
 							               PADRE(2)                                  
 							             /         \                           
 							           nullptr      HIJOD(1)  
@@ -112,7 +112,7 @@ class ArbolAVL{
 							if(r->derecha->factorEquilibrio == 1){
 								r = rotacionDerechaDerecha(r, r->derecha);
 							}
-							/*Caso Derecha-Izquierda (RotaciÛn doble.Padre,hijo y nieto involucrados.):
+							/*Caso Derecha-Izquierda (Rotaci√≥n doble.Padre,hijo y nieto involucrados.):
 							               PADRE(2)
 							              /        \
  									nullptr        HIJOD(-1)         
@@ -132,33 +132,33 @@ class ArbolAVL{
 			}
 			return r;
 		}
-		/*Funciones de los 4 casos de rotaciÛn para equilibrar el ·rbol en caso de que se viole la condiciÛn de equilibrio.*/
+		/*Funciones de los 4 casos de rotaci√≥n para equilibrar el √°rbol en caso de que se viole la condici√≥n de equilibrio.*/
 		
-		/*InserciÛn en el sub·rbol izquierdo de la rama izquierda(rota a la "derecha").*/
+		/*Inserci√≥n en el sub√°rbol izquierdo de la rama izquierda(rota a la "derecha").*/
 		Nodo<T> *rotacionIzquierdaIzquierda(Nodo<T> *padre,Nodo<T> *hijo){
 			padre->izquierda = hijo->derecha;
 			hijo->derecha = padre;
 			if(hijo->factorEquilibrio == -1){ /*El hijo tiene tiene un nodo a la izquierda*/
-				hijo->factorEquilibrio = padre->factorEquilibrio = 0; /*Al hacer la rotaciÛn se equilibra.*/
+				hijo->factorEquilibrio = padre->factorEquilibrio = 0; /*Al hacer la rotaci√≥n se equilibra.*/
 			}else{
 				padre->factorEquilibrio = -1;
 				hijo->factorEquilibrio = 1;
 			}
-			return hijo; /*el hijo apuntar· al padre.*/
+			return hijo; /*el hijo apuntar√° al padre.*/
 		}
-		/*InserciÛn en el sub·rbol derecho de la rama derecha(rota a la "izquierda").*/
+		/*Inserci√≥n en el sub√°rbol derecho de la rama derecha(rota a la "izquierda").*/
 		Nodo<T> *rotacionDerechaDerecha(Nodo<T> *padre,Nodo<T> *hijo){
 			padre->derecha = hijo->izquierda;
 			hijo->izquierda = padre;
 			if(hijo->factorEquilibrio == 1){ /*El hijo tiene un nodo a la derecha.*/
-				padre->factorEquilibrio = hijo->factorEquilibrio = 0; /*Los factores se actualizan a 0,al hacer la rotaciÛn que equilbra el ·rbol.*/
+				padre->factorEquilibrio = hijo->factorEquilibrio = 0; /*Los factores se actualizan a 0,al hacer la rotaci√≥n que equilbra el √°rbol.*/
 			}else{
 				padre->factorEquilibrio = 1;
 				hijo->factorEquilibrio = -1;
 			}
-			return hijo;/*el hijo apuntar· al padre.*/
+			return hijo;/*el hijo apuntar√° al padre.*/
 		}
-		/*InserciÛn en el sub·rbol derecho de la rama izquierda.*/
+		/*Inserci√≥n en el sub√°rbol derecho de la rama izquierda.*/
 		Nodo<T> *rotacionIzquierdaDerecha(Nodo<T> *padre, Nodo<T> *hijo){
 			Nodo<T> *nieto = hijo->derecha;
 			padre->izquierda = nieto->derecha;
@@ -178,7 +178,7 @@ class ArbolAVL{
 			nieto->factorEquilibrio = 0;
 			return nieto;
 		}
-		/*InserciÛn en el sub·rbol izquierdo de la rama derecha.*/
+		/*Inserci√≥n en el sub√°rbol izquierdo de la rama derecha.*/
 		Nodo<T> *rotacionDerechaIzquierda(Nodo<T> *padre, Nodo<T> *hijo){
 			Nodo<T> *nieto = hijo->izquierda;
 		    padre->derecha = nieto->izquierda;
@@ -204,10 +204,10 @@ class ArbolAVL{
 				return false;
 			}else if(padre->dato == d){
 				return true;
-			}else if(padre->dato > d){ /*continua la b˙squeda en la rama izquierda*/
+			}else if(padre->dato > d){ /*continua la b√∫squeda en la rama izquierda*/
 				busqueda(d, padre->izquierda);
 			}else{
-				busqueda(d,padre->derecha);/*continua la b˙squeda en la rama derecha*/
+				busqueda(d,padre->derecha);/*continua la b√∫squeda en la rama derecha*/
 			}
 		}
 		/*Hijo izquierdo,raiz,hijo derecho.*/
